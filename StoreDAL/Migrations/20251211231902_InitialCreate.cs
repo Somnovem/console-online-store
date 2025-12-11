9e0fd1b1f4d800faf8523382e0c5d3f32f2d958d
Artem Zhmura <99594471+Somnovem@users.noreply.github.com>
2025-12-12 01:21:19 +0200

    fix migrations

---
+++ StoreDAL/Migrations/20251211231902_InitialCreate.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StoreDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    category_name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "manufacturers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    manufacturer_name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_manufacturers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_states",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    state_name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_states", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    user_role_name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product_titles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    product_title = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    category_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product_titles", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_product_titles_categories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "categories",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    first_name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    login = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    user_role_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_users_user_roles_user_role_id",
+                        column: x => x.user_role_id,
+                        principalTable: "user_roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    product_title_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    manufacturer_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    unit_price = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    comment = table.Column<string>(type: "TEXT", maxLength: 1000, nullable: false),
+                    available_quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_products_manufacturers_manufacturer_id",
+                        column: x => x.manufacturer_id,
+                        principalTable: "manufacturers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_products_product_titles_product_title_id",
+                        column: x => x.product_title_id,
+                        principalTable: "product_titles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "customer_orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    customer_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    operation_time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    order_state_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer_orders", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_customer_orders_order_states_order_state_id",
+                        column: x => x.order_state_id,
+                        principalTable: "order_states",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_customer_orders_users_customer_id",
+                        column: x => x.customer_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "customer_order_details",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    customer_order_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    product_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    product_amount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer_order_details", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_customer_order_details_customer_orders_customer_order_id",
+                        column: x => x.customer_order_id,
+                        principalTable: "customer_orders",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_customer_order_details_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "order_states",
+                columns: new[] { "id", "state_name" },
+                values: new object[,]
+                {
+                    { 1, "New Order" },
+                    { 2, "Canceled by user" },
+                    { 3, "Canceled by administrator" },
+                    { 4, "Confirmed" },
+                    { 5, "Moved to delivery company" },
+                    { 6, "In delivery" },
+                    { 7, "Delivered to client" },
+                    { 8, "Delivery aproved by client" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "user_roles",
+                columns: new[] { "id", "user_role_name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "Registered" },
+                    { 3, "Guest" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_order_details_customer_order_id",
+                table: "customer_order_details",
+                column: "customer_order_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_order_details_product_id",
+                table: "customer_order_details",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_orders_customer_id",
+                table: "customer_orders",
+                column: "customer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_orders_order_state_id",
+                table: "customer_orders",
+                column: "order_state_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_titles_category_id",
+                table: "product_titles",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_manufacturer_id",
+                table: "products",
+                column: "manufacturer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_product_title_id",
+                table: "products",
+                column: "product_title_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_user_role_id",
+                table: "users",
+                column: "user_role_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customer_order_details");
+
+            migrationBuilder.DropTable(
+                name: "customer_orders");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "order_states");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "manufacturers");
+
+            migrationBuilder.DropTable(
+                name: "product_titles");
+
+            migrationBuilder.DropTable(
+                name: "user_roles");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
